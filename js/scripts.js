478a569b320355a31cfdff402b23b064f3db03b7
zismea <kacp.ziemba@gmail.com>
2024-09-06 15:18:48 +0200

    Introduced changes into JS file
    Introduced Pokemon List with Pokemon stats

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Breakfast';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Ninetales', height: '1.1', types: 'fire'},
+    {name: 'Alakazam', height: '1.5', types: 'psychic'},
+    {name: 'Jynx', height: '1.4', types: ['ice', 'psychic']},
+];
