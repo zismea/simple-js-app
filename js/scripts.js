bdf613034c42b6397de0eef2eb03cf1eb484aa80
zismea <kacp.ziemba@gmail.com>
2024-09-15 13:47:38 +0200

    loop created
    a loop which iterates through each item was created

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 let pokemonList = [
-    {name: 'Ninetales', height: '1.1', types: 'fire'},
-    {name: 'Alakazam', height: '1.5', types: 'psychic'},
-    {name: 'Jynx', height: '1.4', types: ['ice', 'psychic']},
+    {name: 'Ninetales', height: 1, types: 'fire'},
+    {name: 'Alakazam', height: 4, types: 'psychic'},
+    {name: 'Jynx', height: 8, types: ['ice', 'psychic']},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 2) {
+        document.write(pokemonList[i].name + ": Wow, she is tiny!");
+    } else if (pokemonList[i].height > 2 && pokemonList[i].height < 6) {
+        document.write(pokemonList[i].name + ": He is not too dangerous!")
+    } else {
+        document.write(pokemonList[i].name + ": Please be careful!")
+    }
+}
